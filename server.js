43ba4a24fdcad564e83e135f3f1bfa82de8fafb5
Macy Kay Mannix <macymannix@gmail.com>
2022-06-05 17:17:54 -0700

    server notes

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-
+//dependency
 const express = require('express');
 
+//creates express server
 const app = express();
 
+//initial port (localhost:3001)
 const PORT = process.env.PORT || 3001;
 
-const path = require('path');
-
+//parsing data
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//routes
 require('./routes/apiRoutes')(app);
 require('./routes/htmlRoutes')(app);
 
