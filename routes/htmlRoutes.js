47e7796343f193ced6f242357305ad983790fd6e
Macy Kay Mannix <macymannix@gmail.com>
2022-06-05 17:07:27 -0700

    unique id installed

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,13 @@
 const path = require("path");
 
-module.exports = function (app){
+module.exports = function (app) {
+  //GET /notes should return the noteshtml
+  app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  });
 
-//GET /notes should return the noteshtml
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-//GET *wildecard should return the indexhtml
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+  //GET *wildecard should return the indexhtml
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  });
 };
--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 
 const express = require('express');
 
+const app = express();
 
 const PORT = process.env.PORT || 3001;
-const app = express();
-const path = require('path');
 
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+const path = require('path');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
